abc853cda273add8a8bb8ba96f70b99361b9c0f6
Adrian Stratulat <adrian.stratulat91@gmail.com>
2022-06-18 17:44:15 +0300

    initial commit

---
+++ hardware/v1/panelisation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kikit panelize --layout 'grid; rows: 1; cols:2; space: 1mm;' --tabs 'fixed; width: 5mm; hcount: 2' --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm' pico-carrier.kicad_pcb pico-panel.kicad_pcb
